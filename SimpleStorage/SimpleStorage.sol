df0cb53f59ed6f0ab61fed274dbef6e19c77d4a8
AbdullahOmor <omor95011@gmail.com>
2026-02-10 07:06:26 +0600

    Add List of favourite number

--- SimpleStorage/SimpleStorage.sol
+++ SimpleStorage/SimpleStorage.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.18; //this is the solidity version
 
 contract SimpleStorage {
-    uint256 public favouriteNumber;
+    uint256 myFavouriteNumber;
+
+    struct Person{ // Custom type name person
+        uint256 favouriteNumber; // Index 0
+        string name; // Index 1
+    }
+
+    Person[] public listOfPeople; // Dynamic array
 
     function store(uint256 _favouriteNumber) public {
-        favouriteNumber = _favouriteNumber;
+        myFavouriteNumber = _favouriteNumber;
     }
 
     function retrive() public view returns (uint256){
-        return favouriteNumber;
+        return myFavouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name));
     }
 }
