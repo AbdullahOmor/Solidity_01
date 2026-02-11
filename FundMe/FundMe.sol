8af57a08c47ac492f18b3e934a186461186505f0
AbdullahOmor <omor95011@gmail.com>
2026-02-11 20:42:00 +0600

    Add Withdraw Function

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -11,13 +11,49 @@ contract FundMe {
     address[] public funders;
     mapping (address => uint256) public addressToAmountFunded;
 
+    address public owned;
+
+    constructor() {
+        owned = msg.sender;
+    }
+
     function fund() public payable {
         require( msg.value.getConversionRate() >= minimumUsd, "Did not send enough ETH"); // 1e18 = 1 ETH
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] +=  msg.value;
     }
 
-    //function withdraw () public {}
+    function withdraw () public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-    
+        funders = new address[](0); // Reset the array
+
+        /*
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        payable(msg.sender).transfer(address(this).balance); // Send token way-1 (Transfer)
+
+        //way-2 (Send)
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+        */
+
+        //way-3 (Call)
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Send failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owned, "Must be Owner!");
+        _; // First require then rest of code
+
+        /* if
+        _;
+        require(msg.sender == owned, "Must be Owner!");
+        that means
+        first code then require */
+    }
 }
