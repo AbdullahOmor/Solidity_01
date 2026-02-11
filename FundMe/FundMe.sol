9a1201f7753034c4669c0c482f84d8d4b59164f4
AbdullahOmor <omor95011@gmail.com>
2026-02-11 18:57:24 +0600

    Add Chainlink

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    
+    uint256 public minimumUsd = 5e18;
+
+    address[] public funders;
+    mapping (address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd, "Did not send enough ETH"); // 1e18 = 1 ETH
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    //function withdraw () public {}
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); // Chainlink price feed
+        (,int256 price,,,) = priceFeed.latestRoundData(); // Price of ETH in terms of USD
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 }
